e220d9a68c9fc94981b073f2d6d1161415a5357f
DearNoodle <wan619395@gmail.com>
2024-03-01 23:55:32 +0800

    build linked-list class functions

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -80,14 +80,91 @@ class LinkedList {
   }
 
   pop() {
-    if (!this.head) {
-      throw new Error("Cannot pop an empty list");
-    }
+    if (!this.head) throw new Error("Cannot pop in empty list");
     let cur = this.head;
     while (cur.next.next) {
       cur = cur.next;
     }
     cur.next = null;
+  }
+
+  contains(value) {
+    if (!this.head) throw new Error("Empty list");
+    let cur = this.head;
+    if (cur === value) return true;
+    while (cur.next) {
+      cur = cur.next;
+      if (cur.value === value) return true;
+    }
+    return false;
+  }
+
+  find(value) {
+    if (!this.head) throw new Error("Cannot find value in empty list");
+    let nodeIndex = 0;
+    let cur = this.head;
+    if (cur.value === value) return nodeIndex;
+    while (cur.next) {
+      cur = cur.next;
+      nodeIndex += 1;
+      if (cur.value === value) return nodeIndex;
+    }
+    return null;
+  }
+
+  toString() {
+    if (!this.head) throw new Error("Empty list");
+    let linkedListString = "";
+    let cur = this.head;
+    linkedListString += `( ${cur.value} )`;
+    while (cur.next) {
+      cur = cur.next;
+      linkedListString += ` -> ( ${cur.value} )`;
+    }
+    linkedListString += ` -> null`;
+    return linkedListString;
+  }
+
+  insertAt(value, index) {
+    const newNode = new LinkedListNode(value);
+    if (!this.head && index !== 0) throw new Error("List index out of range");
+    let cur = this.head;
+    let pre = null;
+    if (index === 0) {
+      newNode.next = this.head;
+      this.head = newNode;
+      return;
+    }
+    for (let i = 0; i <= index; i++) {
+      if (i === index) {
+        newNode.next = cur;
+        pre.next = newNode;
+        return;
+      }
+      if (!cur.next && i + 1 !== index) {
+        throw new Error("List index out of range");
+      }
+      pre = cur;
+      cur = cur.next;
+    }
+  }
+
+  removeAt(index) {
+    if (!this.head) throw new Error("Cannot remove node from empty list");
+    let cur = this.head;
+    let pre = null;
+    if (index === 0) {
+      this.head = cur.next;
+      return;
+    }
+    for (let i = 0; i < index; i++) {
+      pre = cur;
+      cur = cur.next;
+      if (!cur) {
+        throw new Error("List index out of range");
+      }
+    }
+    pre.next = cur.next;
   }
 }
 
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import LinkedList from "./LinkedList";
+
+const list = new LinkedList();
+list.append(1);
+list.append(2);
+list.append(4);
+list.append(5);
+console.log(list.toString());
+// console.log(list.getHead().value);
+// console.log(list.getTail().value);
+// console.log(list.size());
+// list.pop();
+// console.log(list.at(2).value);
+// console.log(list.find(1));
+// console.log(list.insertAt("value", 2));
+// console.log(list.removeAt(5));
+// console.log(list.toString());
